06c02e67417b0267c246abc08b9bd3b2fe18701d
abbie <abigail.c.holder@gmail.com>
2017-05-23 20:37:14 -0500

    project setup

---
+++ Doordie/app/app.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module("Doordie", ["ngRoute"]);
+
+app.config(["$routeProvider", function ($routeProvider) {
+    $routeProvider.when("/",
+        {
+            templateUrl: "app/login.html",
+            controller: "loginController"
+        })
+        .when("/home",
+        {
+            templateUrl: "app/home.html",
+            controller: "homeController"
+        });
+
+}]);
